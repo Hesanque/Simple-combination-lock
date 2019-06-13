eabd2b30f526fa0955677cf5030c1c0aabdb88ce
hesanque <1151715024@qq.com>
2019-06-13 08:27:16 -0700

    [refactor] add the keil,simulation files with 595
    	-keil4 files
    	-proteus8.6 files
    Update repository files

--- keil4/source_file/shumaguan.c
+++ keil4/source_file/shumaguan.c
@@ -1,5 +1,6 @@
 #include"shumaguan.h"
 
+
 //共阴数码管 段码表
 uchar table_K[18]={0x3f,0x06,0x5b,0x4f,
                 0x66,0x6d,0x7d,0x07,
@@ -9,7 +10,7 @@ uchar table_K[18]={0x3f,0x06,0x5b,0x4f,
 uchar table_A[18]={0xC0,0xF9,0xA4,0xB0,
                 0x99,0x92,0x82,0xF8,
                 0x80,0x90,0x88,0x83,
-                0xC6,0xA1,0x86,0x8E,0x7f,0xff};//0~f，以及小数点，还有不显示。
+                0xC6,0xA1,0x86,0x8E,0x7f,0xff};//0~f，以及小数点，还有不显示。                                                               
 
 /*延时函数*/
 void Delay1ms(uint c)   //误差 0us
@@ -26,44 +27,61 @@ void Delay1ms(uint c)   //误差 0us
 }
 
 
+/*发送一个字节*/
+void HC595SendData(uchar duan)
+{  
+    uchar i;
 
+    //--发送第一个字节--//
+    for(i=0;i<8;i++)
+    {
+        DS = duan>>7;   //从高位到低位
+        duan<<= 1;
+
+        SH_CP = 0;
+        SH_CP = 1;      
+    }
+
+    //--输出--//
+    ST_CP = 0; //set dataline low
+    ST_CP = 1; //片选
+    ST_CP = 0; //set dataline low
+}
 
 /*显示*/
 void led_display(uchar x[])
 {
     uchar duan;
+
+    //显示第一位
     duan=table[x[0]];
-    GPIO_data=duan;
+    HC595SendData(duan);
     wei1=on;
     Delay1ms(1);
     wei1=off;
     Delay1ms(1);
-
-	GPIO_data=0xff;
-	
+    
+    //显示第二位
     duan=table[x[1]];
-    GPIO_data=duan;
+    HC595SendData(duan);
     wei2=on;
     Delay1ms(1);
     wei2=off;
     Delay1ms(1);
-	GPIO_data=0xff;
 
+    //显示第三位
     duan=table[x[2]];
-    GPIO_data=duan;
+    HC595SendData(duan);
     wei3=on;
     Delay1ms(1);
     wei3=off;
     Delay1ms(1);
-	GPIO_data=0xff;
 
-	
+    //显示第四位
     duan=table[x[3]];
-    GPIO_data=duan;
+    HC595SendData(duan);
     wei4=on;
     Delay1ms(1);
     wei4=off;
     Delay1ms(1);
-	GPIO_data=0xff;
-
 }
--- keil4/source_file/shumaguan.h
+++ keil4/source_file/shumaguan.h
@@ -18,26 +18,28 @@
 // #if 1 表示使用 共阴段码表
 #if 0
     
-    #define table table_K       //使用共阴段码表
+    #define table table_K     //使用共阴段码表
 
-    #define on      1
-    #define off     0 
+    #define on      0
+    #define off     1 
 
 #else
 
     #define   table table_A     //使用共阳段码表
 
-    #define on      0 
-    #define off     1 
+    #define on      1 
+    #define off     0 
 #endif
 
+/*定义GPIO引脚*/
+sbit SH_CP=P2^0;    //数据输入时钟线
+sbit DS   =P2^1;     
+sbit ST_CP=P2^2;    //74hc595的spi接口的位声明
 
-/*定义声明变量*/
-#define GPIO_data P1
 sbit wei1=P2^3;
 sbit wei2=P2^4;
 sbit wei3=P2^5;
-sbit wei4=P2^6;//位选
+sbit wei4=P2^6;     //位选
 
 /*函数的声明*/
 void Delay1ms(uint c);
